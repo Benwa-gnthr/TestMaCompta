186dbca00aeca91e7ac01410854335ddc94e25fd
Benwa-gnthr <bgonthier974@gmail.com>
2024-05-06 16:45:38 +0200

    Exercice 1 création des tables

---
+++ database.sql
@@ -0,0 +1,20 @@
+CREATE TABLE iF NOT exists ecritures (
+    uuid VARCHAR(36) PRIMARY KEY,
+    compte_uuid VARCHAR(36),
+    label VARCHAR(255) NOT NULL DEFAULT '',
+    date DATE NULL,
+    type ENUM('C', 'D'),
+    amount DOUBLE(14,2) NOT NULL DEFAULT 0.00,
+    created_at TIMESTAMP NULL DEFAULT current_timestamp(),
+    updated_at TIMESTAMP NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    FOREIGN KEY (compte_uuid) REFERENCES comptes(uuid) ON UPDATE RESTRICT ON DELETE CASCADE
+);
+
+CREATE TABLE iF NOT exists comptes (
+    uuid VARCHAR(36) PRIMARY KEY,
+    login VARCHAR(255) NOT NULL DEFAULT '',
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NULL DEFAULT current_timestamp(),
+    updated_at TIMESTAMP NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+);
